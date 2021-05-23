2e3dc5b7af629ab78d568faf564e81f5b2954140
Jean-Yves Vinet <jean-yves.vinet@cgi.com>
2021-05-23 09:37:04 -0400

    init

---
+++ archive.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"gopkg.in/ini.v1"
+	"jyv.com/archive/util"
+)
+
+type IniFile struct {
+	driver string
+	con    string
+	query  string
+	log    string
+	output string
+}
+
+func logInfo(msg string) {
+	log.Println(msg)
+	fmt.Println(msg)
+}
+
+func logFatal(err interface{}, msg string) {
+	fmt.Println(msg)
+	log.Fatalln(err, msg)
+}
+
+func logPanic(msg string) {
+	fmt.Println(msg)
+	log.Fatalln(msg)
+}
+
+func load_ini_file(filename string) *IniFile {
+	cfg, err := ini.Load(filename)
+	if err != nil {
+		logFatal(err, "Fail to read ini file: "+filename)
+	}
+	inifile := new(IniFile)
+	inifile.driver = cfg.Section("goarchive").Key("driver").String()
+	inifile.con = cfg.Section("goarchive").Key("con").String()
+	inifile.query = cfg.Section("goarchive").Key("query").String()
+	inifile.output = cfg.Section("goarchive").Key("output").String()
+	inifile.log = cfg.Section("goarchive").Key("log").String()
+	return inifile
+}
+
+func validate_inifile(inifile *IniFile) {
+	if len(inifile.driver) == 0 {
+		logPanic("parameter <driver> is mandatory>")
+	}
+	if len(inifile.con) == 0 {
+		logPanic("parameter <con> is mandatory>")
+	}
+	if len(inifile.query) == 0 {
+		logPanic("parameter <query> is mandatory>")
+	}
+	if len(inifile.log) == 0 {
+		inifile.log = "goarchive.log"
+	}
+	if len(inifile.output) == 0 {
+		inifile.output = "goarchive.xlsx"
+	}
+}
+
+func load_parameter() *IniFile {
+	// if first parameter starts init -i or -ini, load ini file and forget about other parameter
+	var inifile *IniFile
+	if len(os.Args) > 1 {
+		v := strings.SplitN(os.Args[1], "=", 2)
+		if len(v) < 2 {
+			logPanic("Syntaxe error")
+		}
+		argname := v[0]
+		arginfo := v[1]
+		if argname == "i" || argname == "--ini" {
+			inifile = load_ini_file(arginfo)
+		} else {
+			inifile = new(IniFile)
+			for i := 1; i < len(os.Args); i++ {
+				v := strings.SplitN(os.Args[i], "=", 2)
+				if len(v) < 2 {
+					logPanic("Syntaxe error")
+				}
+				argname := v[0]
+				arginfo := v[1]
+
+				switch argname {
+				case "d":
+					inifile.driver = arginfo
+				case "--driver":
+					inifile.driver = arginfo
+				case "o":
+					inifile.output = arginfo
+				case "--output":
+					inifile.output = arginfo
+				case "l":
+					inifile.log = arginfo
+				case "--log":
+					inifile.log = arginfo
+				case "q":
+					inifile.query = arginfo
+				case "query":
+					inifile.query = arginfo
+				case "c":
+					inifile.con = arginfo
+				case "--con":
+					inifile.con = arginfo
+				default:
+					logPanic("unknown parameter")
+				}
+			}
+		}
+	} else {
+		logPanic("Syntaxe error")
+	}
+	validate_inifile(inifile)
+	return inifile
+}
+
+func doit(inifile *IniFile) {
+	util.QuerySaveExcel(inifile.driver, inifile.con, inifile.query, inifile.output)
+}
+
+func main() {
+	//syntaxe
+	// d, --driver d={sql driver}
+	// c, --con    c={connection string}
+	// q, --query  q={sql query}
+	// l, --log    l={log file}
+	// o, --output o={output file name
+	// i, --ini    i={initialisation file}
+	//     [goarchive]
+	// 		driver = {sql driver}
+	//      con = { connection string }
+	// 		query = {sql query}
+	// 		log = {log file}
+	// 		output = {output file name
+	inifile := load_parameter()
+	file, err := os.OpenFile(inifile.log, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		logFatal(err, "Error opening log file")
+	}
+	defer file.Close()
+	log.SetOutput(file)
+
+	logInfo("START processing")
+	doit(inifile)
+	logInfo("END processing")
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module jyv.com/archive
+
+go 1.16
+
+require (
+	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/denisenkom/go-mssqldb v0.10.0 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+)
