1571fa3100cefdb5e6c9b2aca349711f0d8e95d7
Jean-Yves Vinet <jean-yves.vinet@cgi.com>
2021-06-02 08:05:47 -0400

    validation json file

--- connection/connection.go
+++ connection/connection.go
@@ -59,17 +59,17 @@ func CreateOrGetDB(name string, driverName string, dataSourceName string) (db *s
 
 func ValidateConnection(con Connection, position int) {
 	if con.Name == "" {
-		log.Fatalln("Connection Error in the json file: <Connections #>", position, "> does not contains the field : <Name>")
+		log.Fatalln("Connection Error in the json file: <Connections #", position, "> does not contains the field : <Name>")
 	}
 	if con.Driver == "" {
-		log.Fatalln("Connection Error in the json file: <Connections #>", position, "> does not contains the field : <Driver>")
+		log.Fatalln("Connection Error in the json file: <Connections #", position, "> does not contains the field : <Driver>")
 	}
 	if con.Driver != "sqlserver" {
 		log.Printf("Connection Error in the json file: <Connections #%d>, the driver <%s> is not supported", position, con.Driver)
 		log.Fatalf("Connection Error in the json file: <Connections #%d>, supported drivers are <sqlserver>", position)
 	}
 	if con.ConnectionString == "" {
-		log.Fatalln("Connection Error in the json file: <Connections #>", position, "> does not contains the field : <ConnectionString>")
+		log.Fatalln("Connection Error in the json file: <Connections #", position, "> does not contains the field : <ConnectionString>")
 	}
 }
 
